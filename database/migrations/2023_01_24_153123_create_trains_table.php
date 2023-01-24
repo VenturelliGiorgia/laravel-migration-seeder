ba7ad8a0e90b6795fec7d461059010605a214941
VenturelliGiorgia <venturelli.giorgia.03@gmail.com>
2023-01-24 17:21:26 +0100

    initial commit

---
+++ database/migrations/2023_01_24_153123_create_trains_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string("azienda");
+            $table->string("stazione_di_partenza");
+            $table->string("stazione_di_arrivo");
+            $table->datatime("orario_di_partenza");
+            $table->datatime("orario_di_arrivo");
+            $table->integer("codice_treno");
+            $table->integer("num_carrozze");
+            $table->boolean("orario")->default(true);
+            $table->boolean("cancellato")->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+};
